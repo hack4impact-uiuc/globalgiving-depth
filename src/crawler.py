ff396e3b462b2925719f3ee42cba600e7ef22c1b
Osama <osamabinesmail@gmail.com>
2019-02-15 17:10:22 -0600

    got calls working

--- src/crawler.py
+++ src/crawler.py
@@ -1,11 +1,24 @@
 import requests
 import json
+import time
 
 headers = {'Accept': 'application/json'}
-r = requests.get("https://api.globalgiving.org/api/public/projectservice/projects/collection/summary/ids?api_key=72ef6e29-cb2b-4613-9cc6-69a88a8d3f3b&projectIds=123,1883", headers=headers)
 
-projects = r.json()['projects']
+nextProjectId = 2
+hasNext = True
+while hasNext:
+    r = requests.get("https://api.globalgiving.org/api/public/projectservice/all/projects/" + 
+                     "?api_key=72ef6e29-cb2b-4613-9cc6-69a88a8d3f3b&nextProjectId=" + str(nextProjectId), 
+                     headers=headers)
 
-for project in projects['project']:
-    print(project)
-    print()
+    projects = r.json()['projects']
+    hasNext = projects['hasNext']
+    if hasNext:
+        nextProjectId = projects['nextProjectId']
+
+    # print(json.dumps(projects, indent=4))
+    for project in projects['project']:
+        print(project['id'])
+        
+    # TODO: time.sleep(60)
+    time.sleep(5)
