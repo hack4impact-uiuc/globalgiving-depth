4acf6ed98fd9f110e6c13ebfd73448875d4d4267
Osama <osamabinesmail@gmail.com>
2019-02-16 04:26:19 -0600

    updated number of orgs

--- src/crawler.py
+++ src/crawler.py
@@ -33,33 +33,39 @@ def parseProjectInfo(project, label):
 headers = {'Accept': 'application/json'}
 
 # JSON files to write to
-labeled_results_json = open('labeled_results.json', 'w')
-unlabeled_results_json = open('unlabeled_results.json', 'w')
+labeled_results_json = open('labeled_results.json', 'a')
+unlabeled_results_json = open('unlabeled_results.json', 'a')
 
 # Initial setup
-nextProjectId = 2
+nextProjectId = 343
 hasNext = True
 labeled_results = []
 unlabeled_results = []
 
-while hasNext:
-    # Requesting projects from Global Giving API
-    r = requests.get("https://api.globalgiving.org/api/public/projectservice/all/projects/" + 
-                     "?api_key=72ef6e29-cb2b-4613-9cc6-69a88a8d3f3b&nextProjectId=" + str(nextProjectId), 
-                     headers=headers)
-    projects = r.json()['projects']
+try:
+    while hasNext:
+        # Requesting projects from Global Giving API
+        r = requests.get("https://api.globalgiving.org/api/public/projectservice/all/projects/" + 
+                        "?api_key=72ef6e29-cb2b-4613-9cc6-69a88a8d3f3b&nextProjectId=" + str(nextProjectId), 
+                        headers=headers)
+        
+        projects = r.json()['projects']
 
-    # Grabbing next projects
-    hasNext = projects['hasNext']
-    if hasNext:
-        nextProjectId = projects['nextProjectId']
+        # Grabbing next projects
+        hasNext = projects['hasNext']
+        if hasNext:
+            nextProjectId = projects['nextProjectId']
 
-    # Recording projects
-    labeled_results += [parseProjectInfo(project, label=True) for project in projects['project']]
-    unlabeled_results += [parseProjectInfo(project, label=False) for project in projects['project']]
+        # Recording projects
+        labeled_results += [parseProjectInfo(project, label=True) for project in projects['project']]
+        unlabeled_results += [parseProjectInfo(project, label=False) for project in projects['project']]
 
-    time.sleep(30)
+        time.sleep(30)
+
+except Exception as e:
+    print(e)
+    print('nextProjectId = ' + str(nextProjectId))
 
 # Writing projects to JSON file
-json.dump({'projects': unlabeled_results}, unlabeled_results_json, sort_keys= True, indent = 2, ensure_ascii = False)
 json.dump({'projects': labeled_results}, labeled_results_json, sort_keys=True, indent = 2, ensure_ascii = False)
+json.dump({'projects': unlabeled_results}, unlabeled_results_json, sort_keys= True, indent = 2, ensure_ascii = False)
