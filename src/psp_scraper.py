47f8a1a0362193077f3b69e0b4ca41315d81ab29
Jacqueline Osborn <jackieo5023@gmail.com>
2019-02-18 22:50:06 -0600

    Add scraping of subpages

--- src/psp_scraper.py
+++ src/psp_scraper.py
@@ -4,6 +4,7 @@ import requests
 import json
 from bs4 import BeautifulSoup
 from bs4.element import Comment
+import re
 
 ## Be able to automate getting links
 ## Add in multiprocessing / some other efficiency improvement
@@ -16,8 +17,18 @@ def main():
     for url in url_list:
         html_doc = requests.get(url).text
         soup = BeautifulSoup(html_doc, "html.parser")
+        other_links = get_other_links(soup, url)
+
+        # Get all text from url and subpages
         text = soup.findAll(text=True)
         text = filter_text(text)
+        for link in other_links:
+            child_html_doc = requests.get(link).text
+            child_soup = BeautifulSoup(child_html_doc, "html.parser")
+            child_text = child_soup.findAll(text=True)
+            child_text = filter_text(child_text)
+            text += child_text
+
         scraping_data.append(text)
 
     with open("scraping_data.txt", "w") as output_file:
@@ -27,7 +38,23 @@ def main():
     output_file.close()
     return
 
+def get_other_links(soup, url):
+    links = []
+    
+    tags = soup.findAll("a", attrs={"target": "_self"})
+    for tag in tags:
+        link = url + "/" + tag.get("href")
+        if link not in links:
+            links.append(link)
 
+    tags = soup.findAll("a", attrs={"href": re.compile(url)})
+    for tag in tags:
+        link = tag.get("href")
+        if link not in links:
+            links.append(tag.get("href"))
+
+    return links
+    
 def filter_text(texts):
     filtered_text = []
     for text in texts:
