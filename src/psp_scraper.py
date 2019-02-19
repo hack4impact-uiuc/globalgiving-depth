bae55ed73798c54678a6edec86ff2bdb34979ec7
Prashant S Pokhriyal <psp@wirelessprv-10-192-237-25.near.illinois.edu>
2019-02-19 02:01:16 -0600

    Takes strings from JSON dictionary, joins text to 1 string, text_scraper can be used as blackbox

--- src/psp_scraper.py
+++ src/psp_scraper.py
@@ -2,34 +2,24 @@ import sys
 
 import requests
 import json
+import multiprocessing as mp
 from bs4 import BeautifulSoup
 from bs4.element import Comment
 import re
 
-## Be able to automate getting links
-## Add in multiprocessing / some other efficiency improvement
-
 def main():
-    input_file = open(sys.argv[1], "r")
-    url_list = input_file.read().splitlines()
+    with open(sys.argv[1], "r") as input_file:
+        input_data = json.load(input_file)
+
+    url_list = []
+    for project in input_data['projects']:
+         if len(project["url"]) != 0:
+             url_list.append(project["url"])
 
     scraping_data = []
     for url in url_list:
-        html_doc = requests.get(url).text
-        soup = BeautifulSoup(html_doc, "html.parser")
-        other_links = get_other_links(soup, url)
-
-        # Get all text from url and subpages
-        text = soup.findAll(text=True)
-        text = filter_text(text)
-        for link in other_links:
-            child_html_doc = requests.get(link).text
-            child_soup = BeautifulSoup(child_html_doc, "html.parser")
-            child_text = child_soup.findAll(text=True)
-            child_text = filter_text(child_text)
-            text += child_text
-
-        scraping_data.append(text)
+        scraping_data.append(text_scraper(url))
+    scraping_data = ''.join(scraping_data)
 
     with open("scraping_data.txt", "w") as output_file:
         json.dump(scraping_data, output_file)
@@ -38,9 +28,32 @@ def main():
     output_file.close()
     return
 
+"""
+text_scraper can be used as a blackbox with a given url.
+It will RETURN a string with text parsed from:
+1) The original site
+2) Associated links
+"""
+def text_scraper(url):
+    assert(type(url) == str)
+    html_doc = requests.get(url).text
+    soup = BeautifulSoup(html_doc, "html.parser")
+    other_links = get_other_links(soup, url)
+
+    # Get all text from url and subpages
+    text = soup.findAll(text=True)
+    text = ''.join(filter_text(text))
+    for link in other_links:
+        child_html_doc = requests.get(link).text
+        child_soup = BeautifulSoup(child_html_doc, "html.parser")
+        child_text = child_soup.findAll(text=True)
+        child_text = ''.join(filter_text(child_text))
+        text += child_text
+    return text
+
 def get_other_links(soup, url):
     links = []
-    
+
     tags = soup.findAll("a", attrs={"target": "_self"})
     for tag in tags:
         link = url + "/" + tag.get("href")
@@ -54,7 +67,7 @@ def get_other_links(soup, url):
             links.append(tag.get("href"))
 
     return links
-    
+
 def filter_text(texts):
     filtered_text = []
     for text in texts:
@@ -68,7 +81,6 @@ def filter_text(texts):
             if len(stripped_text) != 0:
                 filtered_text.append(stripped_text)
     return filtered_text
-
 
 if __name__ == "__main__":
     main()
