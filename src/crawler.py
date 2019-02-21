73b7eb96e1d15ab5c7a5f75100bc764978dd60be
Osama <osamabinesmail@gmail.com>
2019-02-21 15:36:50 -0600

    changed to orgs

--- src/crawler.py
+++ src/crawler.py
@@ -2,6 +2,7 @@ import requests
 import json
 import time
 import os
+import sys
 from dotenv import load_dotenv
 
 
@@ -19,48 +20,46 @@ def main():
     # JSON files to write to
     projects_json = open("projects.json", "w")
 
+    r = requests.get("https://api.globalgiving.org/api/public/orgservice/all/organizations" +
+                    "?api_key=" + 
+                    global_giving_key,
+                    headers=headers)
+
     # Initial setup
-    next_project_id = 2
+    next_org_id = 2
     has_next = True
     projects_list = []
     error_count = 0
 
     # 30000 is so the loop doesn't run forever if we get a 400 because there are around 25000 projects
-    while has_next and next_project_id < 30000:
+    while has_next and next_org_id < 30000:
         # Requesting projects from Global Giving API
-        try:
-            r = requests.get(
-                "https://api.globalgiving.org/api/public/projectservice/all/projects/"
-                + "?api_key="
-                + str(global_giving_key)
-                + "&nextProjectId="
-                + str(next_project_id),
-                headers=headers,
-            )
-            projects = r.json().get("projects")
-            print(next_project_id)
-        except Exception as e:
-            print(e)
-            print(r.status_code)
-            error_count += 1
-            if error_count >= 3:
-                next_project_id += 1
-                error_count = 0
-            break
+    r = requests.get("https://api.globalgiving.org/api/public/orgservice/all/organizations" +
+                    "?api_key=" + 
+                    global_giving_key +
+                    "&nextOrgId=" +
+                    str(next_org_id),
+                    headers=headers)
+
+        projects = r.json().get("projects")
+        print(next_org_id)
+
         if r.status_code != 200:
             print(r.status_code)
             error_count += 1
             if error_count >= 3:
-                next_project_id += 1
+                next_org_id += 1
                 error_count = 0
-            break
+            continue
+
         if projects is None:
-            print("no projects" + str(next_project_id))
+            print("No projects" + str(next_org_id))
             break
+
         # Grabbing next projects
         has_next = projects["hasNext"]
         if has_next:
-            next_project_id = projects["nextProjectId"]
+            next_org_id = projects["nextProjectId"]
 
         # Recording projects
         projects_list += [
@@ -73,8 +72,7 @@ def main():
 
     # Writing projects to JSON file
     json.dump(
-        {"projects": projects_list},
-        projects_json,
+        projects_list,
         sort_keys=True,
         indent=2,
         ensure_ascii=False,
@@ -111,14 +109,12 @@ def parse_project_info(project):
     """
     name = get_project_key(project, ["organization", "name"])
     url = get_project_key(project, ["organization", "url"])
-    main_theme = get_project_key(project, ["themeName"])
     sub_themes = get_project_key(project, ["organization", "themes", "theme"])
-    country = get_project_key(project, ["country"])
+    country = get_project_key(project, ["organization", "country"])
 
     return {
         "name": name,
         "url": url,
-        "mainTheme": main_theme,
         "subThemes": sub_themes,
         "country": country,
     }
