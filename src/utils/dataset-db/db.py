51e971fc59677fbf026dd1d70da874910af47015
Aryn Harmon <achcello@gmail.com>
2019-02-21 22:48:00 -0600

    finish get method

---
+++ src/utils/dataset-db/db.py
@@ -0,0 +1,30 @@
+import os
+import dotenv
+import pymongo
+
+
+def get_dataset(simple=False):
+    """
+    This method retreives the dataset of NGO records from the mongodb instance.
+    Input:
+        simple: we may also retreive simplified records which are only
+        `{"name": name, "url":url}`.
+    Output:
+        dataset: a list containing dictionaries of NGO records
+    """
+    # load the URI from the environment
+    assert dotenv.load_dotenv()
+    uri = os.getenv("MONGO_DB_URI")
+
+    # get the collection from the database
+    client = pymongo.MongoClient(uri)
+    db_collection = client.ggdb.organizations
+    dataset = [org for org in db_collection.find()]
+
+    # pick out only the name and url if we've queried with `simple=True`
+    if simple:
+        dataset = list(
+            map(lambda org: {"name": org["name"], "url": org["url"]}, dataset)
+        )
+
+    return dataset
