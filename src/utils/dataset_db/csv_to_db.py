c55f447a302c8e0be1dee0a0641f08f222dac986
Bhrg <joshDgreat37@gmail.com>
2019-02-27 21:25:44 -0600

    reformatted

--- src/utils/dataset_db/csv_to_db.py
+++ src/utils/dataset_db/csv_to_db.py
@@ -2,14 +2,15 @@ from db import upload_many
 import csv
 import sys
 
+
 def csv_upload(csv_path):
-    '''
+    """
     Given a CSV file, this method uploads it to the organizations-unlabelled
     collection.
-    '''
+    """
     orgs = []
     with open(csv_path, "r") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+        csv_reader = csv.reader(csv_file, delimiter=",")
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
@@ -22,7 +23,7 @@ def csv_upload(csv_path):
                 orgs.append(org)
                 line_count += 1
 
-        print("Records parsed:",line_count)
+        print("Records parsed:", line_count)
 
     upload_many(orgs, db_collection=get_collection("organizations_unlabelled"))
 
