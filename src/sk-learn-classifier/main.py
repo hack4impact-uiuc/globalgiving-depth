e68599cb6b2decdca8bb92187f9a8accf89e4d6e
Jacqueline Osborn <jackieo5023@gmail.com>
2019-03-02 13:00:25 -0600

    black .

--- src/sk-learn-classifier/main.py
+++ src/sk-learn-classifier/main.py
@@ -3,10 +3,15 @@ import json
 import re
 import numpy as np
 
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import (
+    TfidfVectorizer,
+    CountVectorizer,
+    TfidfTransformer,
+)
 from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB
 from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.linear_model import SGDClassifier
+
 
 def get_words(text):
     text = text.lower()
@@ -19,14 +24,17 @@ def get_words(text):
 
     return " ".join(clean_list)
 
+
 def train():
-    with open(sys.argv[1], "r") as input_file:  # training_data.json or ngos_text_113.json
+    with open(
+        sys.argv[1], "r"
+    ) as input_file:  # training_data.json or ngos_text_113.json
         input_data = json.load(input_file)
     print(len(input_data["projects"]))
 
     next_index = 0
-    themes = {} # themes to indices
-    targets = [] # indices of themes, parallel to text array
+    themes = {}  # themes to indices
+    targets = []  # indices of themes, parallel to text array
     text = []
     urls = []
 
@@ -40,20 +48,20 @@ def train():
                 targets.append(themes[theme["id"]])
                 text.append(words)
                 urls.append(project["url"])
-    
-    data= {}
+
+    data = {}
     data["themes"] = themes
     data["targets"] = targets
     data["urls"] = urls
     data["text"] = text
     with open(sys.argv[2], "w") as output_file:  # predictions.json
         json.dump(data, output_file)
-    
+
 
 def classify():
     with open("predictions.json", "r") as input_file:
         training_data = json.load(input_file)
-    with open(sys.argv[3], "r") as input_file: # ../visible-text/scraping_data.json
+    with open(sys.argv[3], "r") as input_file:  # ../visible-text/scraping_data.json
         testing_data = json.load(input_file)
     urls = []
     text = []
@@ -62,17 +70,22 @@ def classify():
             text.append(get_words(project["text"]))
             urls.append(project["url"])
 
-
-    text_clf = Pipeline([
-        ('vect', CountVectorizer(ngram_range=(1,1), stop_words="english", max_df=.6)),
-        ('tfidf', TfidfTransformer()),
-        ('clf', SGDClassifier(max_iter=50, random_state=42))
-    ])
+    text_clf = Pipeline(
+        [
+            (
+                "vect",
+                CountVectorizer(ngram_range=(1, 1), stop_words="english", max_df=0.6),
+            ),
+            ("tfidf", TfidfTransformer()),
+            ("clf", SGDClassifier(max_iter=50, random_state=42)),
+        ]
+    )
     test_words = text
     text_clf.fit(training_data["text"], training_data["targets"])
     predicted = text_clf.predict(test_words)
 
     return predicted
+
 
 if __name__ == "__main__":
     train()
