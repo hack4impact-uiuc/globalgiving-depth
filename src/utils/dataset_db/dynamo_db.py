f0388e0d38c4c37897d2cc3dd9cf380b294ef458
Josh Burke <joshmacintosh@Joshs-MacBook-Pro.local>
2019-03-19 14:08:05 -0500

    db interface working

--- src/utils/dataset_db/dynamo_db.py
+++ src/utils/dataset_db/dynamo_db.py
@@ -1,7 +1,6 @@
 import boto3
 import dotenv
 import os
-import json
 
 
 db_client = None
@@ -14,17 +13,17 @@ def db_init():
     Input:
         None.
     Output:
-        connection: a boto3.dynamodb.client instance representing
+        connection: a boto3.resource instance representing
                            the connection you've created.
     '''
     assert dotenv.load_dotenv()
     AWS_KEY_ID = os.getenv("AWS_KEY_ID")
     AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
-    client = boto3.client(
+    client = boto3.resource(
             'dynamodb',
             aws_access_key_id=AWS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_KEY,
-            region='us-east-2')
+            region_name='us-east-2')
 
     return client
 
@@ -43,13 +42,14 @@ def get_table(client, table_name):
     assert client is not None
     table = tables_by_name.get(table_name)
     if table is None:
-        table = client.get_table(table_name)
+        table = client.Table(table_name)
         assert table is not None
         tables_by_name[table_name] = table
 
     return table
 
-def get_all_items(client, table_name):
+
+def get_all_items(table):
     '''
     Method to retrieve all items from a table.
     Input:
@@ -58,18 +58,35 @@ def get_all_items(client, table_name):
     Output:
         table: a boto.dynamodb.table.Table object specified by table_name
     '''
-    items = []
-    response = {"LastEvaluatedKey": "notNone"}
-    while(response.get("LastEvaluatedKey")):
-        response = client.scan(table_name)
-    return items
+    response = table.scan()
+    data = response['Items']
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+        data.extend(response['Items'])
+    return data
 
 
-def put_many(client, table_name, item_list):
+def get_dataset(table_name):
+    '''
+    This function returns an entire table from a table name. This is the
+    function that should be used in external modules/algorithms.
+    Input:
+        table_name: string, the name of the table you're querying
+    Output:
+        data: list of JSON objects representing the table.
+    '''
+    client = db_init()
+    table = get_table(client, table_name)
+    data = get_all_items(table)
+    return data
+
+
+def put_many(table, item_list):
     '''
     Method to put a list of JSON items in a table.
     Input:
-        table: the name of the table you wish to add to
+        table: the table object you wish to add to
         item_list: a list of JSON structs containing the items' fields
     Output:
         boolean: success or fail
@@ -77,29 +94,40 @@ def put_many(client, table_name, item_list):
     item_count = 0
     for item in item_list:
         try:
-            put_item(client, table_name, item)
-        except(Exception):
+            put_item(table, item)
+        except Exception as e:
             print("failed to upload item at lindex: " +
                   str(item_list.index(item)))
+            print(e)
             continue
+            # remove above continue to add records with missing fields anyways
+            print("Trying again without empty strings")
+            for k, v in item.items():
+                if v == '':
+                    item[k] = 'none'
+            try:
+                put_item(table, item)
+                print("Success!")
+            except Exception as e2:
+                print("failed again.")
+                print(e2)
+                continue
         item_count += 1
     print("Uploaded " + str(item_count) + " / " + str(len(item_list)) +
           " items.")
     return item_count
 
 
-def put_item(client, table_name, item):
+def put_item(table, item):
     '''
     Method to put an individual JSON item in a table.
     Input:
-        client: a boto3.client object
-        table_name: name of table you're adding to
+        table_: table object you're adding to
         item: a JSON struct containing the item's fields
     Output:
-        dict: containing attributes of new item
+        dict: containing metadata
     '''
-    new_item = client.put_item(
-        TableName=table_name,
+    new_item = table.put_item(
         Item=item
     )
     return new_item
