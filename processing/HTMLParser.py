b2ba9593c9c0d9f0709526ef460f8b3aec9b0c57
JoshBurke <joshDgreat37@gmail.com>
2019-04-23 16:11:00 -0500

    Parser refactored

--- processing/HTMLParser.py
+++ processing/HTMLParser.py
@@ -1,4 +1,3 @@
-import sys
 import re
 import json
 import requests
@@ -6,67 +5,98 @@ from bs4 import BeautifulSoup
 from bs4.element import Comment
 
 
-def main():
-    with open(sys.argv[1], "r") as input_file:
-        input_data = json.load(input_file)
+class HTMLParser:
+    def scrape_all_from_file(self, infile, outfile):
+        """
+        This is the only function of the class you should really use.
+        This function takes a file containing JSON records of NGO's, which must
+        include a 'url' field, pulls all text from each URL & recursively from
+        URLs in the same domain, and generates new records with the text in an
+        additional text field. It saves these new records in a new JSON file.
+        Parameters:
+            infile - string: filename of file containing JSON records of NGOs.
+                this JSON file must have a "projects" key which contains a list
+                of NGO records, which each must have a "url" field.
+            outfile - string: filename of a new file to write augmented NGO
+                records with additioinal 'text' field.
+        Returns:
+            None
+        """
+        with open(infile, "r") as input_file:
+            input_data = json.load(input_file)
 
-    scraping_data = {}
-    scraping_data["projects"] = []
-    url_list = []
-    for project in input_data["projects"]:
-        url = project["url"]
-        if url:
-            project_to_add = {}
-            project_to_add["url"] = url
-            project_to_add["text"] = text_scraper(url)
-            scraping_data["projects"].append(project_to_add)
+        scraping_data = {}
+        scraping_data["projects"] = []
+        for project in input_data["projects"]:
+            url = project.get("url")
+            if url:
+                project["text"] = self.scrape_url(url)
+                scraping_data["projects"].append(project)
 
-    with open(sys.argv[2], "w") as output_file:
-        json.dump(scraping_data, output_file)
-    return
+        with open(outfile, "w") as output_file:
+            json.dump(scraping_data, output_file)
 
+    def scrape_url(self, url: str) -> str:
+        """
+        scrape_url can be used as a blackbox with a given url.
+        It will RETURN a string with text parsed from:
+        1) The original site
+        2) Associated links
+        Parameters:
+            url - string: url of page to scrape
+        Returns:
+            text - string: text of original site specified by 'url' along with
+                text from associated links on the page.
+        """
+        try:
+            request = requests.get(url)
+        except requests.exceptions.ConnectionError as e:
+            print(e)
+            return ""
 
-def text_scraper(url: str) -> str:
-    """
-    text_scraper can be used as a blackbox with a given url.
-    It will RETURN a string with text parsed from:
-    1) The original site
-    2) Associated links
-    """
-    try:
-        request = requests.get(url)
-    except requests.exceptions.ConnectionError as e:
-        print(e)
-        return ""
+        html_doc = request.text
+        soup = BeautifulSoup(html_doc, "html.parser")
+        other_links = self.get_other_links(soup, url)
+        # Get all text from url and subpages
+        text = soup.findAll(text=True)
+        text = " ".join(self.filter_text(text))
+        for link in other_links:
+            child_html_doc = requests.get(link).text
+            child_soup = BeautifulSoup(child_html_doc, "html.parser")
+            child_text = child_soup.findAll(text=True)
+            child_text = " ".join(self.filter_text(child_text))
+            text += " "
+            text += child_text
+        return text
 
-    html_doc = request.text
-    soup = BeautifulSoup(html_doc, "html.parser")
-    other_links = get_other_links(soup, url)
-    # Get all text from url and subpages
-    text = soup.findAll(text=True)
-    text = " ".join(filter_text(text))
-    for link in other_links:
-        child_html_doc = requests.get(link).text
-        child_soup = BeautifulSoup(child_html_doc, "html.parser")
-        child_text = child_soup.findAll(text=True)
-        child_text = " ".join(filter_text(child_text))
-        text += " "
-        text += child_text
-    return text
+    def get_other_links(self, soup, url):
+        """
+        This function gets associated links from a webpage.
+        The links should be within the same domain, and not stylesheets.
+        This function is known to have some bugs.
+        Parameters:
+            soup - BeautifulSoup object: should be an html parser
+            url - string: url of page to get associated links from.
+        Returns:
+            links - set: set of intra-domain urls found on 'url' page.
+        """
+        links = set()
 
+        tags = soup.findAll(href=True)
+        regex = re.compile("^" + url)
+        for tag in tags:
+            bad_tags = ["head", "video", "script"]
+            if tag.parent.name in bad_tags:
+                continue
+            sub_url = tag.get("href")
+            if ".css" in sub_url or ".pdf" in sub_url:
+                continue
 
-def get_other_links(soup, url):
-    links = set()
-
-    tags = soup.findAll(href=True)
-    regex = re.compile("^" + url)
-    for tag in tags:
-        sub_url = tag.get("href")
-        if re.match(regex, sub_url):
-            if sub_url not in links:
+            if re.match(regex, sub_url):
                 links.add(sub_url)
-        else:
-            if tag.get("target") == "_self" or tag.get("data-target") == "#":
+            elif tag.get("data-target") == "#" or sub_url.startswith("./"):
+                if sub_url.startswith("./"):
+                    sub_url = sub_url[2:]
                 if sub_url.startswith("/"):
                     sub_url = sub_url[1:]
                     if url.endswith("/"):
@@ -79,23 +109,31 @@ def get_other_links(soup, url):
                     link = url + "/" + sub_url
                     links.add(link)
 
-    return links
+        if url in links:  # safety check
+            links.remove(url)
+        if url + "/" in links:
+            links.remove(url + "/")
+        return links
 
-
-def filter_text(texts):
-    filtered_text = []
-    for text in texts:
-        if not isinstance(text, Comment) and text.parent.name not in {
-            "style",
-            "script",
-            "head",
-            "meta",
-        }:
-            stripped_text = text.strip()
-            if stripped_text:
-                filtered_text.append(stripped_text)
-    return filtered_text
-
-
-if __name__ == "__main__":
-    main()
+    def filter_text(self, texts):
+        """
+        This function filters out tags/scripts from HTML.
+        Parameters:
+            texts - list of strings: list of strings from some URL that should
+                be filtered.
+        Returns:
+            filtered_text - string: concatenated body of text without tags/
+                scripts.
+        """
+        filtered_text = []
+        for text in texts:
+            if not isinstance(text, Comment) and text.parent.name not in {
+                "style",
+                "script",
+                "head",
+                "meta",
+            }:
+                stripped_text = text.strip()
+                if stripped_text:
+                    filtered_text.append(stripped_text)
+        return filtered_text
