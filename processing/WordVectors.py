3248c9a5ef3e35eab3fbe5772e7d22dba4aa2c87
Aryn Harmon <achcello@gmail.com>
2019-04-24 21:21:45 -0500

    make commented changes

--- processing/WordVectors.py
+++ processing/WordVectors.py
@@ -29,7 +29,14 @@ def get_words(text):
 
 class WordVectors:
     """
-    Represent documents as word vectors
+    Represent documents as word vectors. The embeddings for each word correspond
+    with those found in the pretrained word embedding model from spacy. 
+
+    Methods:
+    __init__(self, datafile: str, samples=500)
+    get_word_vectors(self) -> list
+    reduce_word_vectors(self, vectors: list) -> tuple
+    visualize(self, vector_labels: list) -> tuple
     """
 
     dataset = []  # list of dictionaries with "summary" and "_id" fields
@@ -39,19 +46,18 @@ class WordVectors:
     word_vectors = []
     word_embedding = None  # word embedding model; pretrained from spacy.io
 
-    X = None
+    X = None  # feature matrix
     explained_variance = None
 
-    def __init__(self, data_file: str, samples=500):
+    def __init__(self, dataset: list, samples=500):
         """
         Construct WordVectors object and read in the dataset.
-        data_file: string of path to data file
+        dataset: a list of dictionaries which have at least the field "summary"
+            or "text". An "_id" would also be helpful.
         samples: integral number of datapoints to visualize
         """
         self.samples = samples
-
-        with open(data_file, "r") as df:
-            json.dump(self.dataset, df)
+        self.dataset = dataset
         # shuffle the dataset to better train vectors
         random.shuffle(self.dataset)
 
@@ -65,16 +71,24 @@ class WordVectors:
         vectors for each document and normalizes them.
         Returns a list of naive document vectors.
         """
+        # This method was written using the project summaries text body, but
+        # other data sources use "text" as the applicable key. Check this.
+        text_key = "summary"
+        if not self.dataset[key]:
+            key = "text"
         self.word_vectors = [
-            self.word_embedding(remove_stopwords(get_words(doc["summary"]))).vector
+            self.word_embedding(remove_stopwords(get_words(doc[text_key]))).vector
             for doc in self.dataset[: self.samples - 1]
         ]
         return self.word_vectors
 
-    def reduce_word_vectors(self, vectors):
+    def reduce_word_vectors(self, vectors: list, pca_components=2):
         """
         Perform dimensionality reduction on feature matrix using PCA.
-        vectors: list of word vectors
+        Parameters:
+            vectors: list of word vectors
+            PCA_components: an integer (default 2) which is the number of
+                principal components to use for PCA.
         Returns:
             X: reduced feature matrix, numpy.ndarray Nx2
             explained_variance: percentage increase in explained variance due
@@ -82,21 +96,24 @@ class WordVectors:
         """
         X = numpy.vstack(vectors)
         scaler = StandardScaler()
-        scaler.fit_transform(X)
+        scaler.fit_transform(X)  # remove mean and scale to unit variance
         explained_variance = PCA().fit(X).explained_variance_ratio_
         self.explained_variance = explained_variance
-        pca = PCA(n_components=2)
+        pca = PCA(n_components=pca_components)
         pca.fit_transform(X)
         self.X = X
         return X, explained_variance
 
-    def visualize(self, vector_labels):
+    def visualize(self, vector_labels: list):
         """
         Plots the reduced matrix of document embeddings using MatPlotLib.
         Plotted documents are colored according to given labels.
         Also plots a graph of explained variance vs. dimensions
-        Returns:
+        Parameters:
             vector_labels: list of labels for each document
+        Returns:
+            (fig, ax): the figure object and axis object which describes the
+                figure. This is returned so it can be plotted or saved.
         """
         labels = numpy.array(vector_labels)
         fig, ax = plt.subplots(2)
@@ -125,3 +142,4 @@ class WordVectors:
         ax[1].set_xlabel("Number of Principal Components")
         ax[1].set_ylabel("Total Variance Ratio")
         fig.set_size_inches(12, 12)
+        return fig, ax
